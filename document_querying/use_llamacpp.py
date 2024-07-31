d051a4b2365156818050ec4a8d547f77b53fbb88
Paul Seidel <paul.seidel@ableton.com>
2024-07-31 13:47:38 +0200

    Update LlamaCPP example

--- document_querying/use_llamacpp.py
+++ document_querying/use_llamacpp.py
@@ -1,17 +1,18 @@
-import os
-from llama_index import (
+from pathlib import Path
+
+from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
     SimpleDirectoryReader,
     VectorStoreIndex,
-    ServiceContext,
     set_global_tokenizer,
+    Settings,
 )
-from llama_index.embeddings import HuggingFaceEmbedding
-from llama_index.llms import LlamaCPP
-from llama_index.llms.llama_utils import (
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
     messages_to_prompt,
     completion_to_prompt,
 )
@@ -20,7 +21,7 @@ from transformers import AutoTokenizer
 
 DATA_DIR="./data"
 PERSIST_DIR="./storage"
-MODEL_PATH="./models/llama2-7b-layla.Q4_0.gguf"
+MODEL_PATH="./models/llama-2-7b-chat.Q4_K_S.gguf"
 
 MODEL_DEFAULT_OPTIONS = dict(
     temperature=0.1,
@@ -28,24 +29,24 @@ MODEL_DEFAULT_OPTIONS = dict(
     context_window=1024,
     messages_to_prompt=messages_to_prompt,
     completion_to_prompt=completion_to_prompt,
-    verbose=True,
+    model_kwargs={"n_gpu_layers": 24},
 )
 
 def load_model(**options):
     applied_options = MODEL_DEFAULT_OPTIONS | options
     return LlamaCPP(**applied_options)
 
-def create_index(service_context):
-    if not os.path.exists(PERSIST_DIR):
+def create_index():
+    if not Path(PERSIST_DIR).exists():
         # load the documents and create the index
         documents = SimpleDirectoryReader(DATA_DIR).load_data()
-        index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+        index = VectorStoreIndex.from_documents(documents)
         # store it for later
         index.storage_context.persist(persist_dir=PERSIST_DIR)
     else:
         # load the existing index
         storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-        index = load_index_from_storage(storage_context, service_context=service_context)
+        index = load_index_from_storage(storage_context)
     return index
 
 def query(query_engine, question):
@@ -55,13 +56,10 @@ def main():
     set_global_tokenizer(
         AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-chat-hf").encode
     )
-    llm = load_model(model_path=MODEL_PATH)
-    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-    service_context = ServiceContext.from_defaults(
-        llm=llm,
-        embed_model=embed_model,
-    )
-    index = create_index(service_context)
+    Settings.llm = load_model(model_path=MODEL_PATH)
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+    index = create_index()
     query_engine = index.as_query_engine(streaming=True)
 
     while True:
